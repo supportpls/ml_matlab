54d479391263e5680e4e80fd51793217d2b1c553
supportpls <dikonnikov@gmail.com>
2019-02-24 14:34:12 +1300

    Add files via upload
    ex7

---
+++ ex7/findClosestCentroids.m
@@ -0,0 +1,39 @@
+function idx = findClosestCentroids(X, centroids)
+%FINDCLOSESTCENTROIDS computes the centroid memberships for every example
+%   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
+%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   vector of centroid assignments (i.e. each entry in range [1..K])
+%
+
+% Set K
+K = size(centroids, 1);
+counter=(1:K);
+
+% You need to return the following variables correctly.
+idx = zeros(size(X,1), 1);
+
+pass_through = zeros(size(X,1),K);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every example, find its closest centroid, and store
+%               the index inside idx at the appropriate location.
+%               Concretely, idx(i) should contain the index of the centroid
+%               closest to example i. Hence, it should be a value in the 
+%               range 1..K
+%
+% Note: You can use a for-loop over the examples to compute this.
+%
+
+for i = counter,
+  pass_through(:,i)=sum((bsxfun(@minus, X, centroids(i,:))).^2,2);
+  
+end;
+
+[minval, minidx]=min(pass_through,[],2);
+
+idx = minidx;
+
+% =============================================================
+
+end
+
